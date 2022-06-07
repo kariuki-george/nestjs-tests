e8a0fa5743e72e32724d4f7e7ac51af15d8c95a4
smiley-geek <kariukigeorge2030@gmail.com>
2022-06-07 07:36:38 +0000

    Fix: fixed a typo in get.user

--- src/common/graphql.module.ts
+++ src/common/graphql.module.ts
@@ -10,7 +10,7 @@ import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
       autoSchemaFile: './schema.qll',
       debug: true,
       playground: true,
-      introspection:true,
+      introspection: true,
       driver: ApolloDriver,
       //plugins: [ApolloServerPluginLandingPageLocalDefault],
     }),
--- test/user-e2e-spec.ts
+++ test/user-e2e-spec.ts
@@ -8,11 +8,17 @@ import {
   CREATE_USER_OPERATION_NAME,
   generateCreateUserVariables,
 } from './helpers/create.user.helper';
+import {
+  generateGetUserVariable,
+  GET_USER_OPERATION_NAME,
+  GET_USER_QUERY,
+} from './helpers/get.user.helper';
 
 const GRAPHQL_ENDPOINT = '/graphql';
 describe('Users resolver(e2e', () => {
   let app: INestApplication;
   let user: User;
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -39,10 +45,29 @@ describe('Users resolver(e2e', () => {
       .expect((res) => {
         expect(res.body.data.createUser).toBeDefined();
         user = res.body.data.createUser;
+
         expect(user._id).toBeDefined();
         expect(user.firstName).toBe(createUserInput.firstName);
         expect(user.lastName).toBe(createUserInput.lastName);
         expect(user.role).toBe(createUserInput.role);
+      });
+  });
+
+  it('should get the user be the generated userId', () => {
+    return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        operationName: GET_USER_OPERATION_NAME,
+        query: GET_USER_QUERY,
+        variables: generateGetUserVariable(user._id.toString()),
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.user).toBeDefined();
+        expect(res.body.data.user._id).toBeDefined();
+        expect(res.body.data.user.firstName).toBe(user.firstName);
+        expect(res.body.data.user.lastName).toBe(user.lastName);
+        expect(res.body.data.user.role).toBe(user.role);
       });
   });
 });
